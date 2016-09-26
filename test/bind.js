37af24d5afb57f0b39bec28de5093ccab2277a5b
chenan <chenan@dejionline.com>
2016-09-26 14:22:15 +0800

    init

--- test/bind.js
+++ test/bind.js
@@ -1,26 +1,32 @@
 /**
  * Created by root on 16/9/26.
  */
+"use strict";
 
-const logger =require('../utils/logger');
-logger.debug(111);
-Function.prototype.bind = function(){
-    var self =this;
-    var context = [].shift.call(arguments);
-    var args = [].slice.call(arguments);
-    return function () {
-        return self.apply(context,[].concat.call(args,[].slice.call(arguments)));
-    }
-};
+let logger =require('../utils/logger').getLogger('bind');
+
+//自写
+// Function.prototype.bind = function(){
+//     var self =this;
+//     logger.debug(arguments);
+//     var context = [].shift.call(arguments);
+//     logger.debug(context);
+//     var args = [].slice.call(arguments);
+//     logger.debug(args);
+//     return function () {
+//         logger.debug(`inner: ${JSON.stringify(arguments)}`);
+//         return self.apply(context,[].concat.call(args,[].slice.call(arguments)));
+//     }
+// };
 
 var obj = {
     name:'bar'
 };
 
 var func = function(a,b,c,d){
-    logger.debug(this.name);
-    logger.debug(a,b,c,d);
+    // logger.debug(this.name);
+    // logger.debug(a,b,c,d);
     logger.debug(arguments);
 }.bind(obj,1,2);
-
-func.bind(3,4);
+func();
+func(3,4);
