d1d677fb48e35a8cbeec5d10d56af5305b124b1f
chenan <chenan@dejionline.com>
2016-10-08 16:17:00 +0800

    test

--- test/array.js
+++ test/array.js
@@ -7,4 +7,20 @@ let a = [1,2,3].reduce((x,y)=>{
     return x+y
 })
 logger.debug(a);
+logger.debug('-----------------------');
 
+let array =[1,2,3]
+let b =function(x,y){
+    logger.debug(arguments);
+    logger.debug(Array.isArray(arguments));//可判断出类数组对象
+    logger.debug(Array.isArray(array));//可判断出类数组对象
+
+    logger.debug(arguments.toString());//可判断出类数组对象
+    logger.debug(array.toString());//可判断出类数组对象
+
+    // arguments.join(',')//可判断出类数组对象 类数组对象没有继承Array.prototype //arguments.join is not a function
+
+    logger.debug(Array.prototype.splice(arguments,1,1));
+}
+b(1,3)
+logger.debug(typeof array);
--- test/fun.js
+++ test/fun.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 
 const logger =require('../utils/logger').getLogger(__filename.split("/").pop());
 
@@ -6,15 +6,25 @@ const logger =require('../utils/logger').getLogger(__filename.split("/").pop());
 var a ={
     name:'a',
     f1:function(){
-        logger.debug(this.name);//作为方法method被调用时 this指向当前对象
+
+        logger.debug(this);//作为方法method被调用时 this指向当前对象
         return this;
     },
     f2:function () {
         function f3() {
-            logger.debug(this);//作为函数被调用时 this指向严格模式undefined 严格模式下指向全局对象
+            logger.debug(this);//作为嵌套函数被调用时 this指向严格模式undefined 严格模式下指向全局对象
         }
         f3();
         return this;
     }
 }
 a.f1().f2();
+
+
+var B =function(){
+    logger.debug(arguments.callee);// [Function: B]
+    logger.debug(this);//作为嵌套函数被调用时 this指向严格模式undefined 严格模式下指向全局对象
+}
+B();
+
+var c =new a.f1()//作为构造函数调用时 this指向新创建的这个对象即c
