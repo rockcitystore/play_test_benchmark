f86cdd86f1f41dae991bb41ce7aeae6935adec6f
chenan <chenan@dejionline.com>
2016-11-09 10:19:06 +0800

    test

--- test/setimeout.js
+++ test/setimeout.js
@@ -1,9 +1,9 @@
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < 1; i++) {
     console.log(11);
     setTimeout(function () {
         console.log('async ' + i)
     }, 0)
-    setTimeout(console.log('sync ' + i), 1000)
+    setTimeout(function(){console.log('async1000 ' + i)}, 1000)
     console.log(22);
 }
 
